2367773ceb0cdfe7543eeea6c234ba3439726de3
sause668 <soulsitescoding@zohomail.com>
2024-02-10 17:22:45 -0500

    f1

--- problems/01-fruit-data-structure.js
+++ problems/01-fruit-data-structure.js
@@ -1,6 +1,36 @@
 /**************DO NOT MODIFY THIS LINE BELOW*****************/
 const fruits = require('../fruit-data')
 
+const fruits2 = [
+    {
+        "genus": "Malus",
+        "name": "Apple",
+        "id": 6,
+        "family": "Rosaceae",
+        "order": "Rosales",
+        "nutritions": {
+            "carbohydrates": 11.4,
+            "protein": 0.3,
+            "fat": 0.4,
+            "calories": 52,
+            "sugar": 10.3
+        }
+    }, {
+        "genus": "Prunus",
+        "name": "Apricot",
+        "id": 35,
+        "family": "Rosaceae",
+        "order": "Rosales",
+        "nutritions": {
+            "carbohydrates": 3.9,
+            "protein": 0.5,
+            "fat": 0.1,
+            "calories": 15,
+            "sugar": 3.2
+        }
+    }
+]
+
 /* 01. `firstFruitObject()` - Return the first object in the fruits array
 
 console.log(firstFruitObject(fruits));
@@ -10,7 +40,7 @@ console.log(firstFruitObject(fruits));
 */
 
 function firstFruitObject(fruits) {
-    // Your code here 
+    return fruits[0];
 }
 
 
