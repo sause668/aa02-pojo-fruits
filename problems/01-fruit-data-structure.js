b59c225efdd6e030fbe48d8725383d52de874ea6
Andrew <adurbin502@gmail.com>
2024-02-10 17:24:17 -0500

    f2

--- problems/01-fruit-data-structure.js
+++ problems/01-fruit-data-structure.js
@@ -54,7 +54,7 @@ console.log(lastFruitObject(fruits));
 */
 
 function lastFruitObject(fruits) {
-    // Your code here 
+    return fruits[fruits.length - 1];
 }
 
 
@@ -69,7 +69,7 @@ console.log(indexFruitObject(17, fruits));
 */
 
 function indexFruitObject(index, fruits) {
-    // Your code here 
+    // Your code here
 }
 
 
@@ -84,7 +84,7 @@ console.log(fruitNames(fruits));
 */
 
 function fruitNames(fruits) {
-    // Your code here 
+    // Your code here
 }
 
 /* 05. `getFruitKeys()` - Return a list of all of the keys for each fruit record.
@@ -95,7 +95,7 @@ NOTE: Call a function you previously wrote as a helper function.
 */
 
 function getFruitKeys(fruits) {
-    // Your code here 
+    // Your code here
 }
 
 
@@ -109,7 +109,7 @@ console.log(getNutritionsKeys(fruits));
 */
 
 function getNutritionsKeys(fruits) {
-    // Your code here 
+    // Your code here
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
